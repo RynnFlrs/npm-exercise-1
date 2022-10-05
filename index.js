72de6b8f35d07f7d606187021db0194b94d6ac1e
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-06 07:18:03 +0800

    Created VCard QR Generator

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const qrcode = require('qrcode-terminal');
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;', {small:true}));
+
+console.log(qrcode.generate('BEGIN:VCARD\nVERSION:3.0\nFN:Ryann Flores\nTEL;TYPE=work,VOICE:+63-995-292-0869\nEMAIL:ryann.flores@siteminder.com\nEND:VCARD;', {small:true}));
