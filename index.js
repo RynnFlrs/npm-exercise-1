e895fab92dac0c09d6264697b9181805d8a12ba8
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-06 08:18:33 +0800

    created person object for Vcard

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const qrcode = require('qrcode-terminal');
 
+const person = {
+    name: 'Ryann Flores',
+    contactNum: '09952920869',
+    workEmail: 'ryann.flores@siteminder.com',
+    personalEmail: 'ryann.abillon.flores@gmail.com'
+};
+
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;', {small:true}));
 
-console.log(qrcode.generate('BEGIN:VCARD\nVERSION:3.0\nFN:Ryann Flores\nTEL;TYPE=work,VOICE:+63-995-292-0869\nEMAIL:ryann.flores@siteminder.com\nEND:VCARD;', {small:true}));
+console.log(qrcode.generate('BEGIN:VCARD\nVERSION:3.0\n' +
+                            '\nFN:' + person.name +
+                            '\nTEL;TYPE=work,VOICE:' + person.contactNum +
+                            '\nEMAIL;WORK;INTERNET:' + person.workEmail +
+                            '\nEMAIL;HOME;INTERNET:' + person.personalEmail +
+                            '\nEND:VCARD;', {small:true}));
