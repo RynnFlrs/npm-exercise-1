068eb4c3dd6e0ae36f834b6f1fc81cefb3a8208b
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-11 09:20:11 +0800

    Installed and apllied prettier lint

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const qrcode = require('qrcode-terminal');
 
 const person = {
-    name: 'Ryann Flores',
-    contactNum: '09952920869',
-    workEmail: 'ryann.flores@siteminder.com',
-    personalEmail: 'ryann.abillon.flores@gmail.com'
+  name: 'Ryann Flores',
+  contactNum: '(+63)-995-292-0869',
+  workEmail: 'ryann.flores@siteminder.com',
+  personalEmail: 'ryann.abillon.flores@gmail.com',
 };
 
-console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;', {small:true}));
+console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;', {
+    small: true,
+  })
+);
 
-console.log(qrcode.generate('BEGIN:VCARD\nVERSION:3.0\n' +
-                            '\nFN:' + person.name +
-                            '\nTEL;TYPE=work,VOICE:' + person.contactNum +
-                            '\nEMAIL;WORK;INTERNET:' + person.workEmail +
-                            '\nEMAIL;HOME;INTERNET:' + person.personalEmail +
-                            '\nEND:VCARD;', {small:true}));
+console.log(qrcode.generate(
+    'BEGIN:VCARD\nVERSION:3.0\n' +
+      '\nFN:' +
+      person.name +
+      '\nTEL;TYPE=work,VOICE:' +
+      person.contactNum +
+      '\nEMAIL;WORK;INTERNET:' +
+      person.workEmail +
+      '\nEMAIL;HOME;INTERNET:' +
+      person.personalEmail +
+      '\nEND:VCARD;',
+    { small: true }
+  )
+);
